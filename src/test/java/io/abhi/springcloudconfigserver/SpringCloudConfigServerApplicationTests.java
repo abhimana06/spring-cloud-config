9f2f1940f2dc071ce1b5e782469e83e30a37b22d
abhimana06 <abhishek@Alephs-MacBook-Pro.local>
2020-05-19 12:58:38 +0800

    first commit

---
+++ src/test/java/io/abhi/springcloudconfigserver/SpringCloudConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package io.abhi.springcloudconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringCloudConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
